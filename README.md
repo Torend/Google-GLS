0e8850b480829c3ea8f10f07aab3fe028e682de5
Toren Danino <daninotoren@gmail.com>
2020-10-09 19:30:35 +0300

    load scripts with promises and readme
    load scripts with promises
    added README
    fixed input selector type

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Google-GLS
+
 guided learning solution for google.com
+
+### Installation
+
+requires [Python](https://www.python.org/) 3.x+ to run.
+
+Running a simple local HTTP server:
+
+1.open your command prompt (Windows)/ terminal (macOS/ Linux)
+2.navigate to the directory that your example is inside, using the cd command
+3.enter the command to start up the server in that directory:
+
+```sh
+python3 -m http.server
+# On windows try "python" instead of "python3", or "py -3"
+```
+
+Go to [Google](google.com) (make sure the site is in english) open DevTools Console and write:
+
+```sh
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'http://127.0.0.1:8000/player.js';
+document.head.appendChild(script);
+```
+
+### comments
+
+- stepOrdinal and id numbers are not follow
+- CSS dont match
--- player.js
+++ player.js
@@ -1,38 +1,24 @@
-// javascript: (function() {
+const loadScript = (src) => {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.onload = resolve;
+    script.onerror = reject;
+    script.src = src;
+    document.head.append(script);
+  });
+};
 
-//   var url = prompt('file:///C:/Users/Public/Pictures/player.js');
-
-//   if (url) {
-
-//       console.log('Script inject request URL:', url);
-
-//       var script = document.createElement('script');
-//       script.src = url;
-//       document.getElementsByTagName('head')[0].appendChild(script);
-
-//       console.log('Injected script: ', script);
-//   }
-
-// })();
-
-//need to be added before load player.js
-async function addJqueryLib() {
-  var h = document.head;
-  var scr = document.createElement("script");
-  scr.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js";
-  h.appendChild(scr);
-  return;
-}
-
-(async function addJqueryUiLib() {
-  await addJqueryLib();
-  var h = document.head;
-  var scr = document.createElement("script");
-  scr.src =
-    "https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js";
-  h.appendChild(scr);
-  return;
-})();
+loadScript('https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js')
+  .then(() =>
+    loadScript(
+      'https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js'
+    )
+  )
+  .then(() => {
+    main();
+  })
+  .catch(() => console.error('Something went wrong.'));
 
 //////////////////////
 //////////////////////
@@ -50,32 +36,56 @@ function htmlEditor(html, className, value) {
   return html.insert(index, value);
 }
 
-function pressClose(tooltip){
-  $(tooltip).tooltip("close");
+function pressClose(tooltip) {
+  $(tooltip).tooltip('close');
 }
 
-function pressNext(next, curr){
-  $("[title=id-"+next+"]").tooltip("open");
-  $(curr).tooltip("close");
+function pressNext(next, curr) {
+  $('[title=id-' + next + ']').tooltip('open');
+  $(curr).tooltip('close');
 }
 
-function pressBack(back, curr){ // not working (step.id numbers not following)
-  $("[title=id-"+back+"]").tooltip("open");
-  $(curr).tooltip("close");
+function pressBack(back, curr) {
+  // not working for all (step.id numbers are not following)
+  $('[title=id-' + back + ']').tooltip('open');
+  $(curr).tooltip('close');
 }
 
 function addTooltip(step, tip_html, numbersOfSteps) {
-  tip_html = htmlEditor(tip_html, "stepCount", step.action.stepOrdinal);
-  tip_html = htmlEditor(tip_html, "stepsCount", numbersOfSteps);
-  tip_html = htmlEditor(tip_html, "popover-content", step.action.contents["#content"]);
-  let fixedSelector = step.action.selector.replaceAll("\"","");
-  tip_html = htmlEditor(tip_html, "Close", " onClick=\"pressClose(\'"+ fixedSelector + "\')\" ");
-  tip_html = htmlEditor(tip_html, "Next", "onClick=\"pressNext(\'"+ step.followers[0].next + "\', \'" + fixedSelector +"\')\" ");
-  tip_html = htmlEditor(tip_html, "prevB", " onClick=\"pressBack(\'"+ (step.id - 1) + "\', \'" + fixedSelector + "\')\" ");
+  tip_html = htmlEditor(tip_html, 'stepCount', step.action.stepOrdinal);
+  tip_html = htmlEditor(tip_html, 'stepsCount', numbersOfSteps);
+  tip_html = htmlEditor(
+    tip_html,
+    'popover-content',
+    step.action.contents['#content']
+  );
+  let fixedSelector = step.action.selector.replaceAll('"', '');
+  if (fixedSelector.length > 5 && fixedSelector.substring(0, 5) === 'input') {
+    fixedSelector = step.action.selector.replaceAll('"', "\\'");
+  }
+  tip_html = htmlEditor(
+    tip_html,
+    'Close',
+    ' onClick="pressClose(\'' + fixedSelector + '\')" '
+  );
+  tip_html = htmlEditor(
+    tip_html,
+    'Next',
+    'onClick="pressNext(\'' +
+      step.followers[0].next +
+      "', '" +
+      fixedSelector +
+      '\')" '
+  );
+  tip_html = htmlEditor(
+    tip_html,
+    'prevB',
+    ' onClick="pressBack(\'' + (step.id - 1) + "', '" + fixedSelector + '\')" '
+  );
   // console.log(tip_html);
   $(step.action.selector)
     .attr({
-      title: "id-"+step.id,
+      title: 'id-' + step.id,
     })
     .tooltip({
       id: step.uid,
@@ -85,42 +95,43 @@ function addTooltip(step, tip_html, numbersOfSteps) {
         // my: step.action.placement,
         at: step.action.placement,
         of: step.action.selector,
-        collision: "fit"
+        collision: 'fit',
       },
-      hide: { delay: step.action.warningTimeout, effect: 'slide' }
+      hide: { delay: step.action.warningTimeout, effect: 'slide' },
     });
 }
 
 function addCSS(css) {
-  $("<link/>", {
-    rel: "stylesheet",
-    type: "text/css",
-    href: "//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"
-  }).appendTo("head");
-  // let style = `<style>` + css + `</style>`;
-  // document.head.insertAdjacentHTML("beforeend", style);
+  $('<link/>', {
+    rel: 'stylesheet',
+    type: 'text/css',
+    href: '//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css',
+  }).appendTo('head');
+  let style = `<style>` + css + `</style>`;
+  document.head.insertAdjacentHTML('beforeend', style);
 }
 
-//load the json guide
-$.ajax({
-  dataType: "jsonp",
-  url:
-    "https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867",
-  success: function (json) {
-    addCSS(json.data.css);
-    let steps = json.data.structure.steps;
-    let numbersOfSteps = steps.length - 1;
-    steps.forEach((step) => {
-      switch (step.action.type) {
-        case "tip":
-          return addTooltip(step, json.data.tiplates.tip, numbersOfSteps);
-        case "closeScenario":
-          $(".ui-helper-hidden-accessible").remove();
-          $("[title=id-1]").tooltip("open");
-          return;
-        default:
-          return;
-      }
-    });
-  },
-});
+function main() {
+  $.ajax({
+    dataType: 'jsonp',
+    url:
+      'https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867',
+    success: function (json) {
+      addCSS(json.data.css);
+      let steps = json.data.structure.steps;
+      let numbersOfSteps = steps.length - 1;
+      steps.forEach((step) => {
+        switch (step.action.type) {
+          case 'tip':
+            return addTooltip(step, json.data.tiplates.tip, numbersOfSteps);
+          case 'closeScenario':
+            // $('.ui-helper-hidden-accessible').remove();
+            $('[title=id-1]').tooltip('open');
+            return;
+          default:
+            return;
+        }
+      });
+    },
+  });
+}
